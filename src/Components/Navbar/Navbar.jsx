3be191b0bb9fa31767c811b38b5761d55304777d
iM Mustafa <im262897tt@gmail.com>
2024-10-10 07:40:30 +0500

    latest

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -15,9 +15,9 @@ const Navbar = () => {
         <p>E-commerce-Platform</p>
       </div>
       <ul className='nav-menu'>
-        <li onClick={() => {setMenu("shop")}}><Link to='/'>Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
-        <li onClick={() => {setMenu("mens")}}><Link to='/mens'>Mens</Link>{menu === "mens"?<hr/>:<></>}</li>
-        <li onClick={() => {setMenu("womens")}}><Link to='/womens'>Womens</Link>{menu === "womens"?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("shop")}}><Link style={{ textDecoration: 'none' }} to='/'>Shop</Link>{menu === "shop"?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("mens")}}><Link style={{ textDecoration: 'none' }} to='/mens'>Mens</Link>{menu === "mens"?<hr/>:<></>}</li>
+        <li onClick={() => {setMenu("womens")}}><Link style={{ textDecoration: 'none' }} to='/womens'>Womens</Link>{menu === "womens"?<hr/>:<></>}</li>
         <li onClick={() => {setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu === "kids"?<hr/>:<></>}</li>
       </ul>
       <div className='nav-login-cart'>
